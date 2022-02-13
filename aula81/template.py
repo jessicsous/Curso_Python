94c9b70ec03022b21ac23bf85918aa20ce4cfdd1
jessicsous <jessicasilvasousa.eng@gmail.com>
2022-02-12 22:26:34 -0400

    update estudos python

--- aula81/template.py
+++ aula81/template.py
@@ -1,13 +1,16 @@
 from string import Template
 from datetime import datetime
+from email import policy
+
+
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import smtplib
 
-meu_email = 'meuemail@gmail.com'
-minha_senha = 'minhasenha'
+meu_email = 'desousa.jessicasilva@outlook.com'
+minha_senha = '!M@gin320'
 
 with open('template.html', 'r') as html:
     template = Template(html.read())
@@ -15,20 +18,20 @@ with open('template.html', 'r') as html:
     corpo_msg = template.safe_substitute(nome='Lucas Viamonte', data=data_hj)
 
 
-msg = MIMEMultipart()
-msg['from'] = 'Jéssica Silva de Sousa'
-msg['to'] = 'emaildocliente@gmail.com' # cliente
-msg['subject'] = 'assunto do email'
+msg = MIMEMultipart(policy=policy.default)
+msg['from'] = 'desousa.jessicasilva@outlook.com'
+msg['to'] = 'desousa.jessicasilva@gmail.com' # cliente
+msg['subject'] = 'Sou lindo'
 
 
 corpo = MIMEText(corpo_msg, 'html')
 msg.attach(corpo)
 
-with open('imagem.jpg', 'rb') as img:
+with open('image.jpeg', 'rb') as img:
     img = MIMEImage(img.read())
     msg.attach(img)
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+with smtplib.SMTP(host='smtp.office365.com', port=587) as smtp:
     try:
         smtp.ehlo()
         smtp.starttls()
